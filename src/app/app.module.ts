a6804e8b9a6bea5793d32c3977d606089cd9a8a1
Andreea <bociu.andreea@gmail.com>
2023-07-15 18:54:49 +0200

    finished home page, last details needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MenuBarComponent } from './shared-components/menu-bar/menu-bar.component';
-import { MainFooterComponent } from './shared-components/main-footer/main-footer.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LargeInfoBubbleComponent } from './shared-components/large-info-bubble/large-info-bubble.component';
 import { DefaultButtonComponent } from './shared-components/default-button/default-button.component';
 import { CardPagesDisplayComponent } from './shared-components/card-pages-display/card-pages-display.component';
+import { AboutUsCardsComponent } from './components/about-us-cards/about-us-cards.component';
+import { PageFooterComponent } from './shared-components/page-footer/page-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     MenuBarComponent,
-    MainFooterComponent,
     AboutPageComponent,
     LargeInfoBubbleComponent,
     DefaultButtonComponent,
     CardPagesDisplayComponent,
+    AboutUsCardsComponent,
+    PageFooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/about-us-cards/about-us-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-cards',
+  templateUrl: './about-us-cards.component.html',
+  styleUrls: ['./about-us-cards.component.scss']
+})
+export class AboutUsCardsComponent {
+
+}
